9a78ab368d84fe1448867d022413090a43967189
Rowan Seymour <rowanseymour@gmail.com>
2017-01-31 09:33:49 +0200

    Switch back to real django-modeltranslation dependency and just patch get_language method

--- temba/__init__.py
+++ temba/__init__.py
@@ -3,3 +3,8 @@ from __future__ import absolute_import
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .temba_celery import app as celery_app  # noqa
+
+# Patch django-modeltranslation's get_language to not check the active language is configured in Django
+from modeltranslation import utils
+
+utils.get_language = lambda: utils._get_language()
